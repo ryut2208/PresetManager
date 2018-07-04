7ca8ae7ccba1a2a89612d22fc39ec5aef739e6a2
RyutaTanaka <ryut2208@gmail.com>
2018-07-04 17:54:20 +0900

    [add] nullが入らないようにコンストラクタを追加

--- PresetManager/Model/Preset.cs
+++ PresetManager/Model/Preset.cs
@@ -10,6 +10,13 @@ namespace PresetManager.Model
     [DataContract]
     class Preset
     {
+        public Preset()
+        {
+            Title = "";
+            Explain = "";
+            Characters = new List<string>();
+        }
+
         [DataMember(Name="title")]
         public string Title { get; set; }
         [DataMember(Name="explain")]
